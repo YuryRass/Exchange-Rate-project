11b602cbd2d42c5e160ec29651d9129ea94f4227
Yurii <yurchik1997yurchik@gmail.com>
2024-08-10 20:29:21 +0300

    init

---
+++ logger.py
@@ -0,0 +1,25 @@
+import logging
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+LOGGING_FORMAT = os.getenv('LOGGING_FORMAT')
+DEBUG_VALUE = os.getenv('DEBUG', 'False') == 'True'
+if DEBUG_VALUE:
+    LOGGING_LEVEL = "DEBUG"
+else:
+    LOGGING_LEVEL = "INFO"
+
+
+def get_logger(name: str) -> logging.Logger:
+    """Создаем логгер"""
+    logger = logging.getLogger(name)
+    logger.setLevel(LOGGING_LEVEL)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(LOGGING_LEVEL)
+    formatter = logging.Formatter(LOGGING_FORMAT)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    return logger
