b2bb97aa77aa86ca461c8dd49f1a6595ff67891e
Yurii <yurchik1997yurchik@gmail.com>
2024-08-10 10:20:32 +0300

    ignored warning

--- manage.py
+++ manage.py
@@ -2,7 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import warnings
 
+warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 def main():
     """Run administrative tasks."""
