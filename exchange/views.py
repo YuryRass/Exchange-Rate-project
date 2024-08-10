75739aaa58f8d79c7f575d55e9379771678902dc
Yurii <yurchik1997yurchik@gmail.com>
2024-08-10 10:21:53 +0300

    exchange rates view

--- exchange/views.py
+++ exchange/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+from exchange.models import ExchangeRate
+
+def index(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(f"<h1>Обмены курса</h1>")
+
+def rates(request: HttpRequest):
+    exchange_rates = ExchangeRate.objects.all()
+    return render(request, 'exchange/rates.html', {'rates': exchange_rates})
