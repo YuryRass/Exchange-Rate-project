a0159081df5d41f1ee684b304a0bd70027eba87d
Yurii <yurchik1997yurchik@gmail.com>
2024-08-10 08:54:53 +0300

    added async

--- exchange/tasks.py
+++ exchange/tasks.py
@@ -1,8 +1,10 @@
+import asyncio
 from exchange.utils import exchange_rate_task
 
 
 def fetch_exchange_rate():
-    exchange_rate_task.fetch_exchange_rate()
+    asyncio.run(exchange_rate_task.fetch_exchange_rate())
+
 
 def check_exchange_rates():
-    exchange_rate_task.check_exchange_rates()
+    asyncio.run(exchange_rate_task.check_exchange_rates())
--- exchange/utils.py
+++ exchange/utils.py
@@ -1,7 +1,8 @@
 from datetime import datetime, timedelta
 from enum import Enum
-import requests
+import httpx
 from django.utils import timezone
+from asgiref.sync import sync_to_async
 
 from exchange.models import ExchangeRate
 from project.settings import GARANTEX_URL
@@ -14,41 +15,55 @@ class Rates(str, Enum):
 
 class ExchangeRateTask:
 
-    def check_exchange_rates(self) -> None:
-        last_sync = ExchangeRate.objects.last()
+    async def check_exchange_rates(self) -> None:
+        last_sync: ExchangeRate = await self.__get_last_rec()
         if last_sync:
             last_time_sync = last_sync.timestamp
             time_now = timezone.now()
             while time_now - last_time_sync > timedelta(minutes=1):
                 # Если прошло больше 1 минуты с последнего обновления, обновляем курс
                 timestamp = (last_time_sync + timedelta(minutes=1)).timestamp()
-                self.fetch_exchange_rate(timestamp)
+                await self.fetch_exchange_rate(timestamp)
                 last_time_sync += timedelta(minutes=1)
 
-    def fetch_exchange_rate(self, timestamp: float | None = None) -> None:
-        self.__add_exchange_rate(timestamp=timestamp)
+    async def fetch_exchange_rate(self, timestamp: float | None = None) -> None:
+        await self.__add_exchange_rate(timestamp=timestamp)
         # self.__add_exchange_rate(market=Rates.RUBUSDT, timestamp=timestamp)
 
-    def __add_exchange_rate(
+    async def __add_exchange_rate(
         self, market: str = Rates.USDTRUB, timestamp: float | None = None
     ) -> None:
         usdtrub_params = {
-            "market": market,
+            "market": market.value,
             "timestamp": int(timestamp) if timestamp else "",
             "limit": 1,
         }
-        response = requests.get(url=GARANTEX_URL, params=usdtrub_params)
-        response.raise_for_status()
-        rate = response.json()[0].get("price")
+        try:
+            async with httpx.AsyncClient() as client:
+                response = await client.get(url=GARANTEX_URL, params=usdtrub_params)
+                response.raise_for_status()
+                rate = response.json()[0].get("price")
 
-        data = {
-            "rate": float(rate),
-            "currency_from": "USDT" if market == Rates.USDTRUB else "RUB",
-            "currency_to": "RUB" if market == Rates.USDTRUB else "USDT",
-            "timestamp": datetime.fromtimestamp(timestamp) if timestamp else datetime.now()
-        }
+            data = {
+                "rate": float(rate),
+                "currency_from": "USDT" if market == Rates.USDTRUB else "RUB",
+                "currency_to": "RUB" if market == Rates.USDTRUB else "USDT",
+                "timestamp": (
+                    datetime.fromtimestamp(timestamp) if timestamp else timezone.now()
+                ),
+            }
 
-        ExchangeRate.objects.create(**data)
+            await self.__create_rec(**data)
+        except httpx.HTTPStatusError as ex:
+            print(f"HTTP error occurred: {ex}")
+        except Exception as ex:
+            print(f"An error occurred: {ex}")
+
+    async def __get_last_rec(self) -> ExchangeRate | None:
+        return await sync_to_async(ExchangeRate.objects.last)()
+
+    async def __create_rec(self, **data) -> None:
+        await sync_to_async(ExchangeRate.objects.create)(**data)
 
 
 exchange_rate_task = ExchangeRateTask()
